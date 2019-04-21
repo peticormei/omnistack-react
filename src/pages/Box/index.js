d807cc225ef758b4b6c1f0fa177462bf652eec0c
Filipe Mei <peticormei@gmail.com>
2019-04-21 02:28:38 -0300

    first commit

---
+++ src/pages/Box/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { MdInsertDriveFile } from 'react-icons/md'
+import { distanceInWords} from 'date-fns';
+import socket from 'socket.io-client'
+
+import pt from 'date-fns/locale/pt';
+import Dropzone from 'react-dropzone';
+
+import api from '../../services/api';
+
+
+import logo from '../../assets/logo.png'
+import './styles.css';
+
+export default class Box extends Component {
+  state = {
+    box: {}
+  };
+
+  async componentDidMount() {
+    this.subscribeToNewFiles();
+
+    const box = this.props.match.params.id;
+
+    const response = await api.get(`/boxes/${box}`);
+    console.log(response);
+
+    this.setState({ box: response.data });
+  }
+
+  subscribeToNewFiles() {
+    const box = this.props.match.params.id;
+    const io = socket(`https://omnisctak-backend.herokuapp.com`);
+
+    io.emit('connectRoom', box);
+
+    io.on('file', (data) => {
+      this.setState({ box: { ...this.state.box, files: [data, ...this.state.box.files] } });
+    });
+  }
+
+  handleUpload= (files) => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+
+      data.append('file', file);
+      api.post(`boxes/${box}/files`, data);
+    })
+  }
+
+  render() {
+    return (
+      <div id="box-container">
+        <header>
+          <img src={logo} alt=''/>
+          <h1>{this.state.box.title}</h1>
+        </header>
+
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({ getRootProps, getInputProps }) => (
+            <div className='upload' {...getRootProps()}>
+              <input {...getInputProps()} />
+
+              <p>Arraste arquivo ou clique aqui</p>
+            </div>
+          )}
+        </Dropzone>
+
+        <ul>
+          { this.state.box.files && this.state.box.files.map(file => 
+            (<li key={(file._id)}>
+              <a className="fileInfo" href={file.url}>
+                <MdInsertDriveFile size={24} color="#A5Cfff"/>
+                <strong>{file.title}</strong>
+              </a>
+              <span>há{" "}{distanceInWords(file.createdAt, new Date(), { locale: pt })}</span>
+            </li>)
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
